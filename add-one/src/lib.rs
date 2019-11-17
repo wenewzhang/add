782a049af6815116bcf84bf9fe2ca706e1bd8a09
wenewzhang <wenewboy@gmail.com>
2019-11-17 16:15:44 +0800

    test

--- add-one/src/lib.rs
+++ add-one/src/lib.rs
@@ -1,3 +1,17 @@
+use rand::prelude::*;
+
 pub fn add_one(x: i32) -> i32 {
+    let y: u8 = random();
+    println!("{}", y);
     x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(3, add_one(2));
+    }
 }
